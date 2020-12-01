ab0c6b6e898e056a111298c20d72abe3cea2f680
Perkles <otaviocha@gmail.com>
2020-12-01 14:09:58 -0300

    Transforms home page in post index

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-
+from feed.models import Post
 
 def home_page_rendering(request):
     if request.method == 'GET':
-        home = ''
-        return render(request, 'home.html', {'home': home})
+        posts = Post.objects.filter(posted=True).order_by('-posting_date')
+        return render(request, 'home.html', {'posts': posts})
