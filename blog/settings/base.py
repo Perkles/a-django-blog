e58447aa6ae30672ad8409e61d8acb17ea387fbe
Perkles <otaviocha@gmail.com>
2020-12-13 19:32:11 -0300

    Refines template links

--- blog/settings/base.py
+++ blog/settings/base.py
@@ -141,3 +141,6 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
